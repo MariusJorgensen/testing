06caeadf39ee7e6d99012c9ca7bd3e608bf72480
Marius Jorgensen <marjorge@cisco.com>
2018-11-20 10:32:12 +0100

    Updates

---
+++ precache-manifest.0633cc44118557fad696aed2da1a580c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6391bb9a78fbee9e4024",
+    "url": "/testing/static/css/main.0c146d81.chunk.css"
+  },
+  {
+    "revision": "6391bb9a78fbee9e4024",
+    "url": "/testing/static/js/main.6391bb9a.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/testing/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "47854e5355f3c459be53",
+    "url": "/testing/static/js/runtime~main.47854e53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0aa12b5071cb2f1712b50af37e1b2a6f",
+    "url": "/testing/index.html"
+  }
+];

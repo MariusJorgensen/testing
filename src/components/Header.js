37c28694e8f499e03f4a12adba277e1ba1e445b0
Marius Jorgensen <marjorge@cisco.com>
2018-11-21 10:01:38 +0100

    corrected paths

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ function Header() {
     <Wrapper>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/testing/">Home</Link>
         </li>
         <li>
-          <Link to="/randomstring">Random String</Link>
+          <Link to="/testing/randomstring">Random String</Link>
         </li>
       </ul>
     </Wrapper>

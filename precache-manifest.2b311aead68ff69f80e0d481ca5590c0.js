a8b54dafdf3f2431a7c3ec6e4413b7b73bb42f5e
Marius Jorgensen <marjorge@cisco.com>
2018-11-21 15:00:21 +0100

    Updates

---
+++ precache-manifest.2b311aead68ff69f80e0d481ca5590c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bec3cfef7d48347a93b7",
+    "url": "/testing/static/js/main.bec3cfef.chunk.js"
+  },
+  {
+    "revision": "dcebbf8e63954bb80c1b",
+    "url": "/testing/static/js/1.dcebbf8e.chunk.js"
+  },
+  {
+    "revision": "47854e5355f3c459be53",
+    "url": "/testing/static/js/runtime~main.47854e53.js"
+  },
+  {
+    "revision": "673f942463c26cd8251fc88ff3bd1d34",
+    "url": "/testing/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/testing/precache-manifest.a65289bf1d5613b6220fc0b9b4b421ed.js"
+  "/testing/precache-manifest.2b311aead68ff69f80e0d481ca5590c0.js"
 );
 
 workbox.clientsClaim();
